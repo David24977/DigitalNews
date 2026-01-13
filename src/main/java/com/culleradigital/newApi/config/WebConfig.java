6d2ecaa1b752e8b6688116f44b575f2ccaa1cd7e
David <davidferrersapinya@gmail.com>
2026-01-13 11:22:00 +0100

    Update CORS for Vercel frontend

--- src/main/java/com/culleradigital/newApi/config/WebConfig.java
+++ src/main/java/com/culleradigital/newApi/config/WebConfig.java
@@ -1,4 +1,4 @@
-package com.culleradigital.config;
+package com.culleradigital.newApi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
